9e49fbec82cdaec150d4e677183fcf1535168bdd
Srinivasan M <84406070+msv96@users.noreply.github.com>
2021-07-31 21:01:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+async function getData() {
+    const user = await fetch(
+        `https://ipgeolocation.abstractapi.com/v1/?api_key=9a2dbe4826a540e48aff15bb17f86e30`);
+    const data = await user.json();
+
+    document.getElementById('div').innerHTML =
+        `
+        <div class="ip">
+            <div>Your IP</div>
+            <div>${data.ip_address}</div>
+            <div>ISP Provider</div>
+            <div>${data.connection.isp_name}</div>
+            <div>Connection Type</div>
+            <div>${data.connection.connection_type}</div>
+            <div>Time</div>
+            <div>
+            ${data.timezone.current_time}
+            ${data.timezone.abbreviation}
+            ${data.timezone.name}
+            </div>
+        </div>
+        <div class="address">
+            <div>Latitude</div>
+            <div>${data.latitude}</div>
+            <div>Longtitude</div>
+            <div>${data.longitude}</div>
+            <div>City</div>
+            <div>${data.city}</div>
+            <div>Region</div>
+            <div>${data.region}</div>
+            <div>Country</div>
+            <div>${data.country}</div>
+        </div>
+        `;
+}
+getData();
